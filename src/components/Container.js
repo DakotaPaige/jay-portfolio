da0a3fde2bc0aa9780109e868c17444e8d6c95d2
Dakota <dakota.paige@hotmail.com>
2021-06-08 18:42:53 -0700

    Home: Add features images

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,16 +5,13 @@ import { vw, vwTablet, vwDesktop } from 'src/styles/utils';
 import media from 'src/styles/media';
 
 const Container = (props) => {
-  return (
-    <Root>
-      {props.children}
-    </Root>
-  );
+  return <Root>{props.children}</Root>;
 };
 
 const Root = styled.div`
   padding: 0 ${vw(10)};
   margin-top: ${vw(60)};
+  width: 100%;
   @media ${media.tablet} {
     padding: 0 ${vwTablet(40)};
     margin-top: ${vwTablet(80)};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,19 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
 import Page from 'components/Page';
+import Gallery from 'components/Gallery/Gallery';
 
 // https://jaypegsphoto.ca/wp/wp-json/wp/v2/photos?_embed
 
 const Home = () => {
-  const photos = useSelector(state => state.photos.categories);
-
-  console.log(photos);
+  const photos = useSelector((state) => state.photos.categories);
 
   return (
     <Page>
       <Root>
-        <h1>Home</h1>
+        {Object.keys(photos).length > 0 && (
+          <Gallery data={photos['featured']} />
+        )}
       </Root>
     </Page>
   );
